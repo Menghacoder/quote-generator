0f870f4c7c540ec401020254a187bc02812842cf
menghacoder <menghacoder@gmail.com>
2022-09-17 12:47:55 +0100

    adding more feachers to the project (loading and complete loading)

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loder = document.getElementById('loader')
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loder.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loder.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     // authorText.textContent = quote.author;
@@ -25,13 +38,15 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
-
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
     
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -39,11 +54,10 @@ async function getQuotes() {
         // console.log(apiQuotes[12]);
         newQuote();
 
-    } catch (error) {
+    }     catch (error) {
        // Catch Error Here
     }
 }
-
 
 function tweetQuote() {
     const twitterUrl = `http://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
